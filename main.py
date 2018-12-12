197068def2ab778c6c045ed55ba5d439c7eea7b9
Jonathan Tan Jiayi <jonathantanatlol@gmail.com>
2018-12-12 23:38:25 +0800

    completed tutorial
    completed player outline and introduction to the game (tutorial)

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import random
 import time
 import json
-import os
 import speech_recognition as sr
 from player import Player
 
@@ -23,7 +22,7 @@ defense_value -> int (value to divide or subtract by, ignored if not defense typ
 support_class -> int (1-3, 1 = healing, 2 = good effects, 3 = bad effects, ignored if type is not support)
 heal_value -> int (ignored if not support class 1)
 effect -> string (ignored if not good or bad effect support class)
-effect_value -> int (ignored if not good or bad effect support class)
+effect_value -> int (ignored if not support class or not needed)
 backfire_chance -> int (chance to backfire if not yet mastered)
 master_percentage -> int (chance to be mastered upon use)
 mana_consumption -> int (amount of mana consumed)
@@ -51,15 +50,30 @@ def getSpokenSpell():
 
     for word in rawSpeech:
         rawSpeech[rawSpeech.index(word)] = word.lower()
+
+    #pronunced grim-muah
+    if (" ".join(rawSpeech) == "open grimoire"):
+        for spell in THE_PLAYER.getEncounteredSpells():
+            print(SPELL_DATA[spell]["game_name"] + " (Tier: " + str(SPELL_DATA[spell]["tier"]) + ")")
+        getSpokenSpell()
+
+    spellName = "_".join(rawSpeech)
         
-    if not ("_".join(rawSpeech) in VALID_SPELLS):
-        print(" ".join(rawSpeech) + " is not a valid spell. Please try again.")
+    if not (spellName in VALID_SPELLS):
+        print(spellName + " is not a valid spell. Please try again.")
         getSpokenSpell()
-    elif (THE_PLAYER.inUnlockedSpell("_".join(rawSpeech))):
-        return "_".join(rawSpeech)
+    elif (THE_PLAYER.isCastableSpell(spellName)):
+        return spellName
     else:
-        print("You have not unlocked " + SPELL_DATA["_".join(rawSpeech)]["game_name"] + " (Tier " + str(SPELL_DATA["_".join(rawSpeech)]["tier"]) + ")" + " yet. Please try again.")
+        print("You have not unlocked " + SPELL_DATA[spellName]["game_name"] + " (Tier: " + str(SPELL_DATA[spellName]["tier"]) + ")" + " yet. Please try again.")
         getSpokenSpell()
+
+def castSpell(spellName, caster, target = THE_PLAYER):
+    if (caster == THE_PLAYER):
+        print("You cast: " + SPELL_DATA[spellName]["game_name"] + " (Tier: " + str(SPELL_DATA[spellName]["tier"]) + ")")
+        THE_PLAYER.processSpell(spellName)
+        if (target == None):
+            return
 
 #LOADING SPELLS
 for spell in VALID_SPELLS:
@@ -67,9 +81,22 @@ for spell in VALID_SPELLS:
         data = json.load(spellFile)
         SPELL_DATA[spell] = data
 
-spokenSpell = getSpokenSpell()
-spellName = SPELL_DATA[spokenSpell]["game_name"]
-spellTier = SPELL_DATA[spokenSpell]["tier"]
-print("\nYOU CAST:")
-print(spellName + " (Tier " + str(spellTier) + ")")
-os.system("say you cast " + spellName)
+print("Greetings, young mage.")
+time.sleep(1)
+print("Try casting a spell.")
+time.sleep(1)
+print("Do wait for the word \"Ready\" to pop up first.")
+castedSpell = getSpokenSpell()
+castSpell(castedSpell, THE_PLAYER, None)
+time.sleep(1)
+print("Well then, I think I'm done here so I'll be off.")
+time.sleep(1)
+print("Try to tier up as quick as possible to be able to use more spells.")
+time.sleep(1)
+print("Feel free to copy your opponent's spells if you are of a higher or equal tier and share the same attribute.")
+time.sleep(1)
+print("Beware of backfires though, at best you will waste your mana, at worse you will be severely inflicted by your own spell!")
+time.sleep(1)
+print("Don't forget, you can always use \"Open Grimoire\" (grim-muah) to get a list of encountered spells")
+time.sleep(10)
+print("Good luck!")
--- player.py
+++ player.py
@@ -5,12 +5,17 @@ class Player:
     VALID_SPELLS = ["small_fireball", "water_bullet", "cutting_wind", "rock_smash"]
     SPELL_DATA = {}
 
-    unlockedSpells = []
+    encounteredSpells = []
     masteredSpells = []
     health = 100
     attribute = ""
     tier = 0
     mana = 10
+    effects = []
+    defensePoints = 0
+    defensePercentage = 0
+    tierUpgradeProgress = 0
+    tierUpgradeRequirements = [5, 10, 20]
 
     def __init__(self):
         rand = random.randint(0,3)
@@ -33,35 +38,129 @@ class Player:
                     possibleFirstSpells.append(spell)
 
         rand = random.randint(0, len(possibleFirstSpells) - 1)
-        self.unlockedSpells.append(possibleFirstSpells[rand])
+        self.encounteredSpells.append(possibleFirstSpells[rand])
 
         print("Your attribute is: " + self.attribute)
-        print("You have unlocked: " + self.SPELL_DATA[self.unlockedSpells[0]]["game_name"] + " (Tier: 0)")
+        print("You have encountered: " + self.SPELL_DATA[self.encounteredSpells[0]]["game_name"] + " (Tier: 0)")
 
-    def getHealth():
+    def getHealth(self):
         return self.health
 
-    def getAttribute():
+    def getAttribute(self):
         return self.attribute
 
-    def getTier():
+    def getTier(self):
         return self.tier
 
-    def getMana():
+    def getMana(self):
         return self.mana
 
-    def inUnlockedSpell(spellName):
-        return spellName in self.unlockedSpells
+    def getEncounteredSpells(self):
+        return self.encounteredSpells
+
+    def isCastableSpell(self, spellName):
+        return ((self.tier >= self.SPELL_DATA[spellName]["tier"]) & (self.attribute == self.SPELL_DATA[spellName]["attribute"]))
     
-    def isMasteredSpells(spellName):
+    def isMasteredSpell(self, spellName):
         return spellName in self.masteredSpells
 
-    def addUnlockedSpells(spellName):
-        if (spellName in self.unlockedSpells):
+    def addEncounteredSpell(self, spellName):
+        if (spellName in self.encounteredSpells):
             return
-        self.unlockedSpells.append(spellName)
+        self.encounteredSpells.append(spellName)
 
-    def addMasteredSPells(spellName):
+    def addMasteredSpell(self, spellName):
         if (spellName in self.masteredSpells):
             return
         self.masteredSpells.append(spellName)
+
+    def addEffect(self, effectName):
+        self.effects.append(effectName)
+
+    def setHealth(self, health):
+        self.health = health
+
+    def setMana(self, mana):
+        self.mana = mana
+
+    def statsReset(self):
+        self.mana = 10
+        self.health = 100
+        self.effect = []
+
+    #method to deduct health, defense points etc
+    #def processAttack(spellName):
+    
+    #method to process good/bad effects
+    #def processEffects():
+
+    def processSpell(self, spellName):
+        self.mana -= self.SPELL_DATA[spellName]["mana_consumption"]
+        if (self.SPELL_DATA[spellName]["type"] == "offense"):
+            if not (self.isMasteredSpell(spellName)):
+                rand = random.randint(0,99)
+                if (rand > self.SPELL_DATA[spellName]["master_percentage"]):
+                    self.addMasteredSpell(spellName)
+                    print("You have mastered " + self.SPELL_DATA[spellName]["game_name"] + " (Tier: " + str(self.SPELL_DATA[spellName]["tier"]) + ")")
+                    return
+                
+                rand = random.randint(0,99)
+                if (rand > self.SPELL_DATA[spellName]["backfire_chance"]):
+                    temp = self.health
+                    self.health -= self.SPELL_DATA[spellName]["damage"]
+                    print(self.SPELL_DATA[spellname] + " backfired!")
+                    print("Health dropped from " + str(temp) + " to " + str(self.health))
+        elif (self.SPELL_DATA[spellName]["type"] == "defense"):
+            if not (self.isMasteredSpell(spellName)):
+                rand = random.randint(0,99)
+                if (rand > self.SPELL_DATA[spellName]["master_percentage"]):
+                    self.addMasteredSpell(spellName)
+                    print("You have mastered " + self.SPELL_DATA[spellName]["game_name"] + " (Tier: " + str(self.SPELL_DATA[spellName]["tier"]) + ")")
+                else:
+                    rand = random.randint(0,99)
+                    if (rand > self.SPELL_DATA[spellName]["backfire_chance"]):
+                        print(self.SPELL_DATA[spellname] + " backfired!")
+                    else:
+                        if (self.SPELL_DATA[spellName]["defense_class"] == 1):
+                            self.defensePoints += self.SPELL_DATA[spellName]["defense_value"]
+                        else:
+                            self.defensePercentage += self.SPELL_DATA[spellName]["defense_value"]
+                            if (self.defensePercentage > 100):
+                                self.defensePercentage = 100
+            else:
+                if (self.SPELL_DATA[spellName]["defense_class"] == 1):
+                    self.defensePoints += self.SPELL_DATA[spellName]["defense_value"]
+                else:
+                    self.defensePercentage += self.SPELL_DATA[spellName]["defense_value"]
+                    if (self.defensePercentage > 100):
+                        self.defensePercentage = 100
+        else:
+            if not (self.isMasteredSpell(spellName)):
+                rand = random.randint(0,99)
+                if (rand > self.SPELL_DATA[spellName]["master_percentage"]):
+                    self.addMasteredSpell(spellName)
+                    print("You have mastered " + self.SPELL_DATA[spellName]["game_name"] + " (Tier: " + str(self.SPELL_DATA[spellName]["tier"]) + ")")
+                else:
+                    rand = random.randint(0,99)
+                    if (rand > self.SPELL_DATA[spellName]["backfire_chance"]):
+                        print(self.SPELL_DATA[spellname] + " backfired!")
+                        if (self.SPELL_DATA[spellName]["support_class"] == 1):
+                            temp = self.health
+                            self.health -= self.SPELL_DATA[spellName]["heal_value"]
+                            print("Health dropped from " + str(temp) + " to " + str(self.health))
+                        elif (self.SPELL_DATA[spellName]["support_class"] == 3):
+                            self.addEffect(self.SPELL_DATA[spellName]["effect"])
+                    else:
+                        if (self.SPELL_DATA[spellName]["support_class"] == 1):
+                            temp = self.health
+                            self.health -= self.SPELL_DATA[spellName]["heal_value"]
+                            print("Health rose from " + str(temp) + " to " + str(self.health))
+                        elif (self.SPELL_DATA[spellName]["support_class"] == 2):
+                            self.addEffect(self.SPELL_DATA[spellName]["effect"])
+            else:
+                if (self.SPELL_DATA[spellName]["support_class"] == 1):
+                    temp = self.health
+                    self.health -= self.SPELL_DATA[spellName]["heal_value"]
+                    print("Health rose from " + str(temp) + " to " + str(self.health))
+                elif (self.SPELL_DATA[spellName]["support_class"] == 2):
+                    self.addEffect(self.SPELL_DATA[spellName]["effect"])
